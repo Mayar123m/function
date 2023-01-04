cf75f6ae97d31e035972c38781f5a9400c59bdff
mayar123m <Mayar.rady820@gmail.com>
2023-01-04 03:12:55 +0200

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    sayHello()
+
+
+
+}
+fun sayHello(): Unit{
+    println("Hello?")
+    println(typeMe())
+    println("Hello?")
+
+}
+fun typeMe(): String {
+    return "how do u do?"
+}
